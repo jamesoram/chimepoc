f163840a271f0f3ec269e49e70b95767aacb6247
James Oram <herespam1@hotmail.com>
2015-06-01 20:01:51 +0100

    poc v0

--- src/main/java/io/tromba/chimepoc/core/ChimePoc.java
+++ src/main/java/io/tromba/chimepoc/core/ChimePoc.java
@@ -1,5 +1,10 @@
 package io.tromba.chimepoc.core;
 
+import io.tromba.chimepoc.pages.LandingPage;
+import io.tromba.chimepoc.pages.SearchResultsPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by jao on 01/06/15.
  */
@@ -10,6 +15,15 @@ public class ChimePoc {
         String password = args[2];
         String buzzword = args[3];
 
-//        chime(username, password, buzzword);
+        new ChimePoc().chime(username, password, buzzword);
+    }
+    
+    private void chime(String user, String password, String buzzword) {
+        SearchResultsPage page = new LandingPage(getDriver()).enterEmail(user).enterPassword(password).search(buzzword);
+        System.out.println("found " + page.getCandidateCount() + " leads");
+    }
+
+    private WebDriver getDriver() {
+        return new ChromeDriver();
     }
 }
