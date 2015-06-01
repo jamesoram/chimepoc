86142b912ee702e4069f5a59c3d9feaab711d774
James Oram <herespam1@hotmail.com>
2015-06-01 19:32:44 +0100

    add selenium and essential page objects

---
+++ src/main/java/io/tromba/chimepoc/pages/AbstractPage.java
@@ -0,0 +1,21 @@
+package io.tromba.chimepoc.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by jao on 01/06/15.
+ */
+public abstract class AbstractPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public AbstractPage(WebDriver driver) {
+        this.driver = driver;
+        this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        this.wait = new WebDriverWait(driver, 30);
+    }
+}
---
+++ src/main/java/io/tromba/chimepoc/pages/LandingPage.java
@@ -0,0 +1,13 @@
+package io.tromba.chimepoc.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by jao on 01/06/15.
+ */
+public class LandingPage extends AbstractPage {
+
+    public LandingPage(WebDriver driver) {
+        super(driver);
+    }
+}
